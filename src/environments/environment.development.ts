e5c524df8bf1eb17a717829d361741cc5287323c
Alejo <rafael.valencia.r4@gmail.com>
2024-10-04 12:30:12 -0500

    git ignore

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    firebaseConfig: {
+        apiKey: "AIzaSyA9fWHNUQMkpgL1V0NjJ1mj_ARf1GXwxG4",
+        authDomain: "dejavuoh-fire.firebaseapp.com",
+        projectId: "dejavuoh-fire",
+        storageBucket: "dejavuoh-fire.appspot.com",
+        messagingSenderId: "181692919420",
+        appId: "1:181692919420:web:d1e624f3f9d6742b4e63f9",
+        measurementId: "G-MC64SHVKF6"
+    }
+};
