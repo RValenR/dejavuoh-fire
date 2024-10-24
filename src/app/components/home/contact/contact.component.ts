1d99bf8eb0bd8ee125fdcddeeee96610b9163e25
Alejo <rafael.valencia.r4@gmail.com>
2024-10-24 16:15:29 -0500

    uso cmoponente bloque de equipo

--- src/app/components/home/contact/contact.component.ts
+++ src/app/components/home/contact/contact.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { TopbarComponent } from '../../commons/topbar/topbar.component';
+import { TeamBarComponent } from '../../commons/team-bar/team-bar.component';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [TopbarComponent],
+  imports: [TopbarComponent, TeamBarComponent],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css'
 })
 export class ContactComponent {
-
+  mostrarBotonTeamBar = true;
 }
--- src/app/components/home/information/information.component.ts
+++ src/app/components/home/information/information.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { TopbarComponent } from '../../commons/topbar/topbar.component';
+import { TeamBarComponent } from '../../commons/team-bar/team-bar.component';
+import { faL } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-information',
   standalone: true,
-  imports: [TopbarComponent],
+  imports: [TopbarComponent, TeamBarComponent],
   templateUrl: './information.component.html',
   styleUrl: './information.component.css'
 })
 export class InformationComponent {
-
+  mostrarBotonTeamBar = false;
 }
