ac9dd44aaab6cb3462e5db38763a580f3044864c
Alejo <rafael.valencia.r4@gmail.com>
2024-10-25 00:03:48 -0500

    bloque contacto con variables personalizadas

--- src/app/components/commons/contact-block/contact-block.component.ts
+++ src/app/components/commons/contact-block/contact-block.component.ts
@@ -1,16 +1,19 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-contact-block',
   standalone: true,
-  imports: [ReactiveFormsModule, CommonModule],
+  imports: [ReactiveFormsModule, CommonModule, RouterModule],
   templateUrl: './contact-block.component.html',
   styleUrl: './contact-block.component.css'
 })
 export class ContactBlockComponent {
   @Input() showImage: boolean = false;
+  @Input() showForm: boolean = false;
+  @Input() costumHeigh: string = "408px";
   formulario: FormGroup = this.fb.group({}); ;
 
   constructor(private fb: FormBuilder) {}
--- src/app/components/home/information/information.component.ts
+++ src/app/components/home/information/information.component.ts
@@ -2,14 +2,17 @@ import { Component } from '@angular/core';
 import { TopbarComponent } from '../../commons/topbar/topbar.component';
 import { TeamBarComponent } from '../../commons/team-bar/team-bar.component';
 import { faL } from '@fortawesome/free-solid-svg-icons';
+import { ContactBlockComponent } from '../../commons/contact-block/contact-block.component';
+import { FooterComponent } from '../../commons/footer/footer.component';
 
 @Component({
   selector: 'app-information',
   standalone: true,
-  imports: [TopbarComponent, TeamBarComponent],
+  imports: [TopbarComponent, TeamBarComponent, ContactBlockComponent, FooterComponent],
   templateUrl: './information.component.html',
   styleUrl: './information.component.css'
 })
 export class InformationComponent {
   mostrarBotonTeamBar = false;
+  costumHeight = "200px"
 }
