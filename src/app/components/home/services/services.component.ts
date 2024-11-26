7ed52ad96747e2d90b4621e5a13cc6be13343ae6
Alejo <rafael.valencia.r4@gmail.com>
2024-11-26 09:57:44 -0500

    servicios modo desktop

--- src/app/components/home/services/services.component.ts
+++ src/app/components/home/services/services.component.ts
@@ -1,12 +1,34 @@
 import { Component } from '@angular/core';
 import { TopbarComponent } from '../../commons/topbar/topbar.component';
+import { CommonModule } from '@angular/common';
+import { FooterComponent } from '../../commons/footer/footer.component';
+import { ContactBlockComponent } from '../../commons/contact-block/contact-block.component';
 @Component({
   selector: 'app-services',
   standalone: true,
-  imports: [TopbarComponent],
+  imports: [TopbarComponent, CommonModule, ContactBlockComponent, FooterComponent],
   templateUrl: './services.component.html',
   styleUrl: './services.component.css'
 })
 export class ServicesComponent {
 
+  showMarketing: boolean = true;
+  showDesarrolloWeb: boolean = false;
+  isActiveDM:boolean = true;
+  isActiveWeb:boolean=false;
+  costumHeight = "200px"
+
+  mostrarDigitalMarketing(){
+    this.showMarketing = true;
+    this.showDesarrolloWeb = false;
+    this.isActiveDM = true;
+    this.isActiveWeb = false;
+  }
+
+  mostrarDesarrolloWeb(){
+    this.showDesarrolloWeb = true;
+    this.showMarketing = false;
+    this.isActiveDM = false;
+    this.isActiveWeb = true;
+  }
 }
